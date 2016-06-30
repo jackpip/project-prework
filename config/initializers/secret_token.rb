a0bc001acc6daed3837cc04a6ec27c7b236faac6
Jack Pipitone <pipitone.jack@gmail.com>
2016-06-30 17:25:07 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectPrework::Application.config.secret_key_base = '3fa9ba556c49d18b338178603f23d9c4451e22f9e86863762778ddc2c48423ec207e651a58acccd4e7fb9668c97230cb5e50213ad673ea2cafe05e8f944c65f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectPrework::Application.config.session_store :cookie_store, key: '_project-prework_session'
